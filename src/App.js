d8280efce2b51d83f291453fc0acb1a7d3e6b946
ShernNing <sherningtan@gmail.com>
2023-08-02 11:19:19 +0800

    init

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { themeContext } from "./Context";
 import { lazy, Suspense, useContext } from "react";
 import ScrollToTop from "react-scroll-to-top";
 import Heading from "./Components/Heading/Heading";
-import { BrowserRouter } from "react-router-dom";
 
 const IntroLazy = lazy(() => import('./Components/Intro/Intro'));
 const ServicesLazy = lazy(() => import('./Components/Services/Services'));
@@ -22,46 +21,44 @@ function App() {
   const darkMode = theme.state.darkMode;
 
   return (
-    <BrowserRouter>
-      <div className="App" style={{background: darkMode? 'black': '', color: darkMode? 'white': ''}}>
-        <Navbar/>
-        <Heading/>
-        <Suspense fallback={<div>Loading...</div>}>
-          <IntroLazy/>
-          <ServicesLazy/>
-          <PortfolioLazy/>
-          <ContactLazy/>
-        </Suspense>
-        <ScrollToTop smooth className="ScrollToTop"/>
+    <div className="App" style={{background: darkMode? 'black': '', color: darkMode? 'white': ''}}>
+      <Navbar/>
+      <Heading/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <IntroLazy/>
+        <ServicesLazy/>
+        <PortfolioLazy/>
+        <ContactLazy/>
+      </Suspense>
+      <ScrollToTop smooth className="ScrollToTop"/>
 
-        {/* <Routes>
-          <Route path='heading' element={<Heading/>}/>
-          <Route path="intro" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <IntroLazy/>
-            </Suspense>
-          }/>
-          <Route path="services" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <ServicesLazy/>
-            </Suspense>
-          }/>
-          <Route path="portfolio" element={
-            <Suspense fallback='Loading...'>
-              <PortfolioLazy/>
-            </Suspense>
-          }/>
-          <Route path="contact" element={
-            <Suspense fallback='Loading...'>
-              <ContactLazy/>
-            </Suspense>
-          }/>
-        </Routes> */}
-        {/* <Experience/> */}
-        {/* <Works/> */}
-        {/* <Footer/> */}
-      </div>
-    </BrowserRouter>
+      {/* <Routes>
+        <Route path='heading' element={<Heading/>}/>
+        <Route path="intro" element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <IntroLazy/>
+          </Suspense>
+        }/>
+        <Route path="services" element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <ServicesLazy/>
+          </Suspense>
+        }/>
+        <Route path="portfolio" element={
+          <Suspense fallback='Loading...'>
+            <PortfolioLazy/>
+          </Suspense>
+        }/>
+        <Route path="contact" element={
+          <Suspense fallback='Loading...'>
+            <ContactLazy/>
+          </Suspense>
+        }/>
+      </Routes> */}
+      {/* <Experience/> */}
+      {/* <Works/> */}
+      {/* <Footer/> */}
+    </div>
   );
 };
 
