fba4ad96c3ec91525a8329b69d1688acf7bc947f
ShernNing <sherningtan@gmail.com>
2023-08-17 22:37:09 +0800

    removal of joke

--- src/Components/Joke/Joke.js
+++ src/Components/Joke/Joke.js
@@ -19,15 +19,50 @@ function Joke() {
     e.preventDefault();
     fetchJoke();
   }
+
+
+
+  //feedbackform
+  const [score, setScore] = useState('4');
+  const [comment, setComment] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Number(score) <5 && comment.length < 10) {
+      alert('Please enter a comment');
+      return
+    }
+    console.log('Form submitted')
+    setComment('');
+    setScore('5');
+  }
+
+
+  const isDisabled = Number(score) < 5 && comment.length <= 10;
+
+  const textArea = isDisabled ? "Feedback please" : 'thank you'
   
 
   return (
     <div>
       <h1>The Joke Generator</h1>
       <h3>{joke}</h3>
-      <form>
-        <button onClick={handleGenerateJoke}>Generate Joke</button>
-      </form>
+      <button onClick={handleGenerateJoke}>Generate Joke</button>
+
+
+
+      {/* feedbackform */}
+      <div className="contactForm">
+        <form action="submit" onSubmit={handleSubmit}>
+          <fieldset>
+            <h2>Score: {score}</h2>
+            <input type="range" min={0} max={10} value={score} onChange={(e) => setScore(e.target.value)} />
+            <label htmlFor="comment">Comments: </label>
+            <textarea name="comment" id="comment" cols="30" rows="10" placeholder={textArea} value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
+            <button type='submit' disabled={isDisabled}>Submit</button>
+          </fieldset>
+        </form>
+      </div>
     </div>
   )
 }
